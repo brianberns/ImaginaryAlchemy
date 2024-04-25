ffbb285a3d25ea6e91e62b2851757dea792b7629
Brian Berns <brianberns@gmail.com>
2024-04-25 09:41:35 -0400

    Add project files.

---
+++ ImaginaryAlchemy/Model.fs
@@ -0,0 +1,48 @@
+﻿namespace LlamaSharpTest
+
+open FSharp.Control
+
+open LLama.Abstractions
+open LLama.Common
+open LLama
+
+module Model =
+
+    let private modelPath = @"C:\Users\brian\Downloads\Meta-Llama-3-8B.Q8_0.gguf"
+
+    let private combineRaw (executor : ILLamaExecutor) =
+
+        let antiPrompt = "User:"
+        let inferenceParams =
+            InferenceParams(
+                Temperature = 0.0f,
+                AntiPrompts = [antiPrompt])
+
+        fun (conceptA : string) (conceptB : string) ->
+            if conceptA = conceptB then
+                Some conceptA
+            else
+                let (conceptA, conceptB) =
+                    min conceptA conceptB,
+                    max conceptA conceptB
+                let text =
+                    $"Transcript of a dialog, where the User interacts with an assistant named Bob. In each interaction, the user provides two concepts, and Bob combines them into new concept. Bob is creative and always responds with a single word, even when it seems like there is no word to represent the combined concept.\nUser: Fire, Water\nBob: Steam\nUser: {conceptA}, {conceptB}\nBob: "
+                let concept =
+                    executor.InferAsync(text, inferenceParams)
+                        |> AsyncSeq.ofAsyncEnum
+                        |> AsyncSeq.fold (+) ""
+                        |> Async.RunSynchronously
+                let concept = concept.TrimEnd()
+                let concept =
+                    if concept.EndsWith(antiPrompt) then
+                        concept.Substring(0, concept.Length - antiPrompt.Length)
+                    else concept
+                let concept = concept.Trim()
+                if concept.Contains(' ') then None
+                else Some concept
+
+    let combine =
+        let parameters = ModelParams(modelPath, GpuLayerCount=100)
+        let model = LLamaWeights.LoadFromFile(parameters)
+        let executor = StatelessExecutor(model, parameters)
+        combineRaw executor
---
+++ ImaginaryAlchemy/Program.fs
@@ -0,0 +1,103 @@
+﻿namespace LlamaSharpTest
+
+open System
+
+type Term = string
+
+type Response =
+    {
+        Result : Term
+        IsNew : bool
+    }
+
+type TermMap = Map<Term (*result*), Option<Term (*first*) * Term (*second*)>>
+
+module Program =
+
+    let getMessages exn =
+
+        let rec loop (exn : exn) =
+            seq {
+                match exn with
+                    | :? AggregateException as aggExn ->
+                        for innerExn in aggExn.InnerExceptions do
+                            yield! loop innerExn
+                    | _ ->
+                        if not (isNull exn.InnerException) then
+                            yield! loop exn.InnerException
+                yield exn.Message
+            }
+
+        loop exn
+            |> Seq.toArray
+
+    let pickOne =
+        let rng = Random(1)
+        fun (terms : _[]) ->
+            let idx = rng.Next(terms.Length)
+            terms[idx]
+
+    let pickTwo (asked : Set<_>) (terms : _[]) =
+
+        let rec loop () =
+            let first, second =
+                let first = pickOne terms
+                let second = pickOne terms
+                if first <= second then first, second
+                else second, first
+            if asked.Contains(first, second) then
+                loop ()
+            else
+                first, second
+
+        if asked.Count < (terms.Length * (terms.Length + 1)) / 2 then
+            loop ()
+        else
+            failwith "No remaining combinations"
+
+    let dump term (termMap : TermMap) =
+
+        let rec loop indent seen term =
+            if Set.contains term seen then seen
+            else
+                match termMap[term] with
+                    | Some (first, second) ->
+                        printfn $"{String(' ', 3 * indent)}{term} = {first} + {second}"
+                        let seen = Set.add term seen
+                        let indent = indent + 1
+                        let seen = loop indent seen first
+                        let seen = loop indent seen second
+                        seen
+                    | None -> seen
+
+        Console.ForegroundColor <- ConsoleColor.Green
+        printfn ""
+        loop 0 Set.empty term
+            |> ignore
+        Console.ForegroundColor <- ConsoleColor.White
+
+    let increment (asked : Set<Term * Term>) (termMap : TermMap) =
+        let first, second =
+            let terms = Seq.toArray termMap.Keys
+            pickTwo asked terms
+        let asked' = asked.Add(first, second)
+        match Model.combine first second with
+            | Some term ->
+                let termMap' =
+                    Map.add
+                        term
+                        (Some (first, second))
+                        termMap
+                dump term termMap'
+                asked', termMap'
+            | None -> asked', termMap
+
+    let rec loop asked termMap : unit =
+        let asked', termMap' = increment asked termMap
+        loop asked' termMap'
+
+    Console.OutputEncoding <- System.Text.Encoding.UTF8
+    [ "Water"; "Fire"; "Wind"; "Earth" ]
+        |> Seq.map (fun term -> term, None)
+        |> Map
+        |> loop Set.empty
