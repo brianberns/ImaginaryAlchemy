64317c354f983acbe93e7abe96eadf061df722c0
Brian Berns <brianberns@gmail.com>
2024-04-26 16:37:44 -0400

    Rewrote to be deterministic.

--- ImaginaryAlchemy/Program.fs
+++ ImaginaryAlchemy/Program.fs
@@ -4,88 +4,100 @@ open System
 
 type Term = string
 
-type TermMap = Map<Term (*result*), Option<Term (*first*) * Term (*second*)>>
+type TermMap = Map<Term, Option<Term * Term>>
+
+type Inventory =
+    {
+        OldTermMap : TermMap
+        NewTermMap : TermMap
+    }
+
+module Inventory =
+
+    let private uncurry f (a, b) = f a b
+
+    let private keySet map =
+        map |> Map.keys |> set
+
+    let create oldTermMap newTermMap =
+        assert(
+            Set.intersect
+                (keySet oldTermMap)
+                (keySet newTermMap) = Set.empty)
+        assert(
+            [
+                yield! Map.values oldTermMap |> Seq.choose id
+                yield! Map.values newTermMap |> Seq.choose id
+            ]
+                |> List.unzip
+                |> uncurry List.append
+                |> Seq.forall (fun term ->
+                    oldTermMap.ContainsKey(term)))
+        {
+            OldTermMap = oldTermMap
+            NewTermMap = newTermMap
+        }
+
+    let iterate inv =
+        let allTerms =
+            keySet inv.OldTermMap
+                + keySet inv.NewTermMap
+        let pairs =
+            seq {
+                for newTerm in inv.NewTermMap.Keys do
+                    for term in allTerms do
+                        yield newTerm, term
+            }
+        let oldTermMap =
+            Map [
+                yield! Map.toSeq inv.OldTermMap
+                yield! Map.toSeq inv.NewTermMap
+            ]
+        let newTermMap =
+            (Map.empty, pairs)
+                ||> Seq.fold (fun acc (first, second) ->
+                    match Model.combine first second with
+                        | Some term when
+                            allTerms
+                                |> Set.contains term
+                                |> not ->
+                            acc.Add(term, Some (first, second))
+                        | _ -> acc)
+        create oldTermMap newTermMap
+
+    let dump inv =
+
+        let entries =
+            inv.NewTermMap
+                |> Map.toSeq
+                |> Seq.choose (fun (term, pairOpt) ->
+                    pairOpt
+                        |> Option.map (fun pair ->
+                            term, pair))
+
+        Console.ForegroundColor <- ConsoleColor.Green
+        for term, (first, second) in entries do
+            printfn $"{term} = {first} + {second}"
+        Console.ForegroundColor <- ConsoleColor.White
 
 module Program =
 
-    let rng = Random(0)
-
-    let pickOne terms =
-        terms
-            |> Array.length
-            |> rng.Next
-            |> Array.get terms
-
-    let pickTwo (asked : Set<_>) (terms : _[]) =
-
-        let rec loop () =
-            let first, second =
-                let first = pickOne terms
-                let second = pickOne terms
-                min first second, max first second
-            if first = second || asked.Contains(first, second) then
-                loop ()
-            else
-                first, second
-
-        if asked.Count < (terms.Length * (terms.Length - 1)) / 2 then
-            loop ()
-        else
-            failwith "No remaining combinations"
-
-    let dump term (termMap : TermMap) =
-
-        let rec loop indent seen term =
-            if Set.contains term seen then seen
-            else
-                match termMap[term] with
-                    | Some (first, second) ->
-                        printfn $"{String(' ', 3 * indent)}{term} = {first} + {second}"
-                        let seen = Set.add term seen
-                        let indent = indent + 1
-                        let seen = loop indent seen first
-                        let seen = loop indent seen second
-                        seen
-                    | None -> seen
-
-        Console.ForegroundColor <- ConsoleColor.Green
-        printfn ""
-        loop 0 Set.empty term
-            |> ignore
-        Console.ForegroundColor <- ConsoleColor.White
-
-    let increment (asked : Set<Term * Term>) (termMap : TermMap) =
-        let first, second =
-            let terms = Seq.toArray termMap.Keys
-            pickTwo asked terms
-        let asked' = asked.Add(first, second)
-        match Model.combine first second with
-            | Some term when not (termMap.ContainsKey(term)) ->
-                let termMap' =
-                    Map.add
-                        term
-                        (Some (first, second))
-                        termMap
-                dump term termMap'
-                asked', termMap'
-            | _ -> asked', termMap
-
-    let rec loop asked termMap : unit =
-        let asked', termMap' = increment asked termMap
-        loop asked' termMap'
-
-    try
-        Console.OutputEncoding <- System.Text.Encoding.UTF8
-        let asked =
-            set [ "Fire", "Water" ]
-        let termMap =
+    let inv =
+        let oldTermMap =
             Map [
                 "Fire", None
                 "Water", None
-                "Stone", None
-                "Wood", None
+            ]
+        let newTermMap =
+            Map [
+                "Earth", None
+                "Wind", None
                 "Steam", Some ("Fire", "Water")
             ]
-        loop asked termMap
+        Inventory.create oldTermMap newTermMap
+
+    Console.OutputEncoding <- System.Text.Encoding.UTF8
+    try
+        Inventory.iterate inv |> Inventory.dump
     with exn ->
         printfn $"{exn.Message}"
