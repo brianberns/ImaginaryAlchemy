b3414fb482c7aafd50635d0492ab76681c735b6c
Brian Berns <brianberns@gmail.com>
2024-04-29 00:29:32 -0400

    Began work on view rendering.

--- Client/src/App.fs
+++ Client/src/App.fs
@@ -5,7 +5,12 @@ open Elmish
 open Elmish.React
 open Feliz
 
-type Model = List<ConceptInfo>
+type Model =
+    {
+        ConceptInfos : List<ConceptInfo>
+        First : ConceptInfo
+        Second : ConceptInfo
+    }
 
 type Msg = Msg
 
@@ -13,17 +18,23 @@ module Model =
 
     let init () =
         let model =
-            [
-                "Earth", 0
-                "Fire", 0
-                "Water", 0
-                "Air", 0
-                "Steam", 1
-            ] |> List.map (fun (concept, gen) ->
-                {
-                    Concept = concept
-                    Generation = gen
-                })
+            let conceptInfos =
+                [
+                    "Earth", 0
+                    "Fire", 0
+                    "Water", 0
+                    "Air", 0
+                    "Steam", 1
+                ] |> List.map (fun (concept, gen) ->
+                    {
+                        Concept = concept
+                        Generation = gen
+                    })
+            {
+                ConceptInfos = conceptInfos
+                First = conceptInfos.Head
+                Second = conceptInfos.Head
+            }
         model, Cmd.none
 
     let update msg model =
@@ -34,12 +45,22 @@ module View =
 
     let renderConceptInfo info =
         Html.div [
-            Html.span info.Concept
-            Html.span info.Generation
+            prop.className "concept-info"
+            prop.children [
+                Html.span [
+                    prop.className "concept"
+                    prop.text info.Concept
+                ]
+                Html.span [
+                    prop.className "generation"
+                    prop.innerHtml
+                        (String.replicate info.Generation "&bull;")
+                ]
+            ]
         ]
 
     let render model dispatch =
-        model
+        model.ConceptInfos
             |> Seq.map renderConceptInfo
             |> Html.div
 
