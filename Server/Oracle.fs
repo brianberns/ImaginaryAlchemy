11ff771e674700b8142d80c50262186cac35ccb3
Brian Berns <brianberns@gmail.com>
2024-05-09 01:40:56 -0400

    Minor refactor and commented.

--- Server/Oracle.fs
+++ Server/Oracle.fs
@@ -16,37 +16,46 @@ type Settings =
         ApiKey : string
     }
 
-type Oracle =
+/// Inference oracle.
+type Oracle private =
     {
+        /// GPT service.
         Service : OpenAIService
+
+        /// Set of all possible concepts.
         ConceptSet : Set<Concept>
     }
 
 module Oracle =
 
+    /// GPT prompt template.
     [<Literal>]
     let private promptTemplate =
         """The following is a list of imaginary alchemy experiments. Each experiment combines two concepts into a new concept. The new concept is always a singular noun.
 > Fire + Water = Steam
 > %s + %s = """
 
+    /// Normlizes the given concept. E.g. "water" -> "Water".
     let private normalize (concept : Concept) : Concept =
         concept[0..0].ToUpper() + concept[1..].ToLower()
 
+    /// Creates an oracle using the list of possible concepts in the
+    /// given directory.
     let create dir =
 
-        let settings =
-            let path = Path.Combine(dir, "appsettings.json")
-            ConfigurationBuilder()
-                .AddJsonFile(path)
-                .Build()
-                .Get<Settings>()
-
+            // connect to GPT service
         let service =
+            let settings =
+                let path = Path.Combine(dir, "appsettings.json")
+                ConfigurationBuilder()
+                    .AddJsonFile(path)
+                    .Build()
+                    .Get<Settings>()
             OpenAiOptions(ApiKey = settings.ApiKey)
                 |> OpenAIService
 
-        // https://www.reddit.com/r/learnprogramming/comments/4yoap9/large_word_list_of_english_nouns/
+            // load set of all possible concepts
+            // https://www.reddit.com/r/learnprogramming/comments/4yoap9/large_word_list_of_english_nouns
         let conceptSet =
             let path = Path.Combine(dir, "nouns.txt")
             File.ReadLines(path)
@@ -57,10 +66,6 @@ module Oracle =
             Service = service
             ConceptSet = conceptSet
         }
-
-    let private isValid oracle concept =
-        concept = normalize concept
-            && oracle.ConceptSet.Contains(concept)
 
     /// Infers the combination of two concepts.
     let private infer oracle (first : Concept) (second : Concept) =
@@ -89,11 +94,12 @@ module Oracle =
         else
             failwith resp.Error.Message
 
+    /// Converts the given concept to singular, if necessary.
     let private trySingular oracle (concept : Concept) =
 
         let test suffix =
             if concept.EndsWith(suffix : string) then
-                let concept' =
+                let concept' : Concept =
                     concept.Substring(
                         0,
                         concept.Length - suffix.Length)
@@ -112,24 +118,20 @@ module Oracle =
     let combine oracle (first : Concept) (second : Concept) =
 
             // check for valid input
-        if isValid oracle first
-            && isValid oracle second then
+        if oracle.ConceptSet.Contains(first)
+            && oracle.ConceptSet.Contains(second) then
 
+                // can't combine a concept with itself
             if first = second then
                 Error first
             else
-
                     // normalize concept order
                 let first, second =
                     min first second,
                     max first second
 
                     // combine concepts
-                let concept =
-                    if first = "Fire" && second = "Water" then   // hard-coded example
-                        "Steam"
-                    else
-                        infer oracle first second
+                let concept = infer oracle first second
 
                     // accept result?
                 match trySingular oracle concept with
