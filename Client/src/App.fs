8cf8afcab745a470ecdd24080367a8ed56758a8e
Brian Berns <brianberns@gmail.com>
2024-05-09 13:45:18 -0400

    Commented.

--- Client/src/App.fs
+++ Client/src/App.fs
@@ -5,6 +5,7 @@ open Elmish.React
 
 module App =
 
+        // Elmish go!
     Program.mkProgram Model.init Model.update View.render
         |> Program.withReactSynchronous "elmish-app"
         |> Program.run
