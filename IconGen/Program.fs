3ca2efffd451679c795e3034052cd713951cc8b1
Brian Berns <brianberns@gmail.com>
2024-05-23 01:01:19 -0400

    Switched to The Noun Project.

--- IconGen/Program.fs
+++ IconGen/Program.fs
@@ -1,44 +1,21 @@
 ﻿namespace ImaginaryAlchemy
 
-open System
-open System.IO
-
-open OpenAI
-open OpenAI.Managers
-open OpenAI.ObjectModels
-open OpenAI.ObjectModels.RequestModels
+open RestSharp
+open RestSharp.Authenticators
 
 module Program =
 
-    [<Literal>]
-    let private promptTemplate =
-        "Abstract, low detail, 2D icon of %s on a white background"
-
-    let service =
-        let settings = Settings.get @"..\..\..\..\Server\"
-        new OpenAIService(
-            OpenAiOptions(ApiKey = settings.ApiKey))
-
-    for concept in ["earth"; "air"; "fire"; "water"] do
-
-        let prompt = sprintf promptTemplate concept
-        let req =
-            ImageCreateRequest(prompt,
-                Model = Models.Dall_e_3,
-                N = 1,
-                Size = "1024x1024",
-                ResponseFormat = "b64_json")
-
-        let resp =
-            service.Image
-                .CreateImage(req)
-                .Result
-
-        if resp.Successful then
-            let path = $"{prompt}.png"
-            let bytes =
-                let result = Seq.exactlyOne resp.Results
-                Convert.FromBase64String(result.B64)
-            File.WriteAllBytes(path, bytes)
-        else
-            printfn $"{resp.Error.Message}"
+    let settings = Settings.get "."
+    let options =
+        RestClientOptions(
+            "https://api.thenounproject.com",
+            Authenticator = OAuth1Auth.ForRequestToken(
+                settings.ApiKey, settings.Secret))
+    let client = new RestClient(options)
+    let request =
+        RestRequest("/v2/icon/")
+            .AddQueryParameter("query", "water")
+            .AddQueryParameter("thumbnail_size", "42")
+            .AddQueryParameter("include_svg", "1")
+    let response = client.ExecuteGet(request)
+    printfn "%A" response.Content
---
+++ IconGen/Settings.fs
@@ -0,0 +1,23 @@
+﻿namespace ImaginaryAlchemy
+
+open System.IO
+open Microsoft.Extensions.Configuration
+
+/// Server-side settings.
+type Settings =
+    {
+        ApiKey : string
+        Secret : string
+    }
+
+module Settings =
+
+    /// Gets settings from the given directory.
+    let get dir =
+        let path =
+            Path.Combine(dir, "appsettings.json")
+                |> Path.GetFullPath
+        ConfigurationBuilder()
+            .AddJsonFile(path)
+            .Build()
+            .Get<Settings>()
